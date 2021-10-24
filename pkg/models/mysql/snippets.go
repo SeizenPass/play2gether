996d43fae2d7cb605fd2e2229fb193e6a340ae63
Amiran Kurman <amiranfox@gmail.com>
2021-10-24 12:00:06 +0600

    Initial lol

---
+++ pkg/models/mysql/snippets.go
@@ -0,0 +1,96 @@
+package mysql
+
+import (
+	"database/sql"
+	"errors"
+
+	"github.com/kwhitlock/lets-go-book/pkg/models"
+)
+
+// SnippetModel Snippet struct
+type SnippetModel struct {
+	DB *sql.DB
+}
+
+// Insert snippets into DB
+func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
+	stmt := `INSERT INTO snippets (title, content, created, expires)
+	VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
+
+	transaction, err := m.DB.Begin()
+	if err != nil {
+		return 0, err
+	}
+
+	result, err := transaction.Exec(stmt, title, content, expires)
+	if err != nil {
+		transaction.Rollback()
+		return 0, nil
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		transaction.Rollback()
+		return 0, err
+	}
+	err = transaction.Commit()
+	if err != nil {
+		return 0, err
+	}
+
+	return int(id), nil
+
+}
+
+// Get func
+func (m *SnippetModel) Get(id int) (*models.Snippet, error) {
+	stmt := `SELECT id, title, content, created, expires FROM snippets
+	WHERE expires > UTC_TIMESTAMP() AND id = ?`
+
+	row := m.DB.QueryRow(stmt, id)
+
+	// init a pointer to new Snippet struct
+	s := &models.Snippet{}
+
+	err := row.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, models.ErrNoRecord
+		} else {
+			return nil, err
+		}
+	}
+
+	return s, nil
+
+}
+
+// Latest func
+func (m *SnippetModel) Latest() ([]*models.Snippet, error) {
+	stmt := `SELECT id, title, content, created, expires FROM snippets
+	WHERE expires > UTC_TIMESTAMP() ORDER BY created DESC LIMIT 10`
+
+	rows, err := m.DB.Query(stmt)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	snippets := []*models.Snippet{}
+
+	for rows.Next() {
+		s := &models.Snippet{}
+		err = rows.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+		if err != nil {
+			return nil, err
+		}
+		snippets = append(snippets, s)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return snippets, nil
+}
